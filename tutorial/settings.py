215652cb919e1f2acf366408833f18d7de07eebe
shyatu <67035613+shyatu@users.noreply.github.com>
2022-02-15 15:53:01 +0800

    Update tutorial/settings.py

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -28,6 +28,11 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -37,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
